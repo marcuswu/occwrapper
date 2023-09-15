3f80bdb8027c4cfbf4b112c834b1bb1008124303
Marcus Wu <mwu@digitaltorque.com>
2023-09-15 15:24:46 -0400

    Add gp_Circ Location()

--- include/occwrapper/gp_circ.h
+++ include/occwrapper/gp_circ.h
@@ -6,6 +6,7 @@ extern "C" {
 #endif
     gpCirc gpCirc_Init(gpAx2 center, double radius);
     double gpCirc_Radius(gpCirc circle);
+    gpPnt gpCirc_Location(gpCirc circle);
     void gpCirc_Free(gpCirc circle);
 #ifdef __cplusplus
 }
--- src/gp_circ.cpp
+++ src/gp_circ.cpp
@@ -12,6 +12,13 @@ double gpCirc_Radius(gpCirc circle) {
     return circ->Radius();
 }
 
+gpPnt gpCirc_Location(gpCirc circle) {
+    gp_Circ *circ = (gp_Circ *)circle;
+    gp_Pnt *ret = new gp_Pnt();
+    *ret = circ->Location();
+    return (void*) ret;
+}
+
 void gpCirc_Free(gpCirc circle) {
     gp_Circ *c = (gp_Circ *)circle;
     delete c;
